fa71126e718a8bdff1e856216ca3b6fafcc73831
Aleksandr Beloushkin <abeloushkin@neoflex.ru>
2021-06-30 17:10:37 +0300

    Adds getting candles graph command

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module tg_skillbox
 go 1.15
 
 require (
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/joho/godotenv v1.3.0
+
+    gonum.org/v1/plot v0.9.0 
+    github.com/pplcc/plotext v0.0.0-20180221170324-68ab3c6e05c3
+ 
+
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,21 +4,42 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/bitly/go-simplejson"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
+	"github.com/pplcc/plotext/custplotter"
+	"gonum.org/v1/plot"
 )
 
 const currency = "RUB"
+const picFileName = "candlesticks.png"
 
 type bnResp struct {
 	Price float64 `json:"price,string"`
 	Code  int64   `json:"code"`
+}
+
+// Kline define kline info
+type Kline struct {
+	OpenTime                 int64  `json:"openTime"`
+	Open                     string `json:"open"`
+	High                     string `json:"high"`
+	Low                      string `json:"low"`
+	Close                    string `json:"close"`
+	Volume                   string `json:"volume"`
+	CloseTime                int64  `json:"closeTime"`
+	QuoteAssetVolume         string `json:"quoteAssetVolume"`
+	TradeNum                 int64  `json:"tradeNum"`
+	TakerBuyBaseAssetVolume  string `json:"takerBuyBaseAssetVolume"`
+	TakerBuyQuoteAssetVolume string `json:"takerBuyQuoteAssetVolume"`
 }
 
 type wallet map[string]float64
@@ -68,8 +89,14 @@ func main() {
 		case "SHOW":
 			answer := showWallet(chatId)
 			bot.Send(tgbotapi.NewMessage(chatId, answer))
+		case "GRAPH":
+			err := getGraph(command, currency, chatId, bot)
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(chatId, "Error when trying to send candles Graph"))
+			}
+
 		default:
-			bot.Send(tgbotapi.NewMessage(chatId, "Команда не найдена!"))
+			bot.Send(tgbotapi.NewMessage(chatId, "No such command!"))
 		}
 
 	}
@@ -174,6 +201,123 @@ func getPrice(symbol string, currency string) (price float64, err error) {
 	}
 
 	price = jsonResp.Price
+	return
+}
+
+func newJSON(data []byte) (j *simplejson.Json, err error) {
+	j, err = simplejson.NewJson(data)
+	if err != nil {
+		return nil, err
+	}
+	return j, nil
+}
+
+func getLowestTime() int64 {
+	return (time.Now().Add(-24 * time.Hour)).UnixNano() / int64(time.Millisecond)
+}
+
+func getGraph(command []string, currency string, chatId int64, bot *tgbotapi.BotAPI) (err error) {
+
+	if len(command) != 2 {
+		return fmt.Errorf("Incorrect command: %v", command)
+	}
+
+	symbol := command[1]
+
+	var currencySuffix string
+	if currency == "USD" {
+		currencySuffix = "T"
+	}
+
+	apiUrl := fmt.Sprintf("https://api.binance.com/api/v3/klines?symbol=%s%s%s&interval=30m&startTime=%d", symbol, currency, currencySuffix, getLowestTime())
+	resp, err := http.Get(apiUrl)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	j, err := newJSON(data)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	num := len(j.MustArray())
+
+	res := make(custplotter.TOHLCVs, num)
+	for i := 0; i < num; i++ {
+		item := j.GetIndex(i)
+		if len(item.MustArray()) < 11 {
+			return fmt.Errorf("invalid kline response")
+		}
+		// Time
+		res[i].T = float64(item.GetIndex(0).MustInt64())
+		// Open
+		res[i].O, err = strconv.ParseFloat(item.GetIndex(1).MustString(), 64)
+		if err != nil {
+			return fmt.Errorf("invalid kline response")
+		}
+		// Highest
+		res[i].H, err = strconv.ParseFloat(item.GetIndex(2).MustString(), 64)
+		if err != nil {
+			return fmt.Errorf("invalid kline response")
+		}
+		// Lowest
+		res[i].L, err = strconv.ParseFloat(item.GetIndex(3).MustString(), 64)
+		if err != nil {
+			return fmt.Errorf("invalid kline response")
+		}
+		// Close
+		res[i].C, err = strconv.ParseFloat(item.GetIndex(4).MustString(), 64)
+		if err != nil {
+			return fmt.Errorf("invalid kline response")
+		}
+		// Volume
+		res[i].V, err = strconv.ParseFloat(item.GetIndex(5).MustString(), 64)
+		if err != nil {
+			return fmt.Errorf("invalid kline response")
+		}
+	}
+
+	p := plot.New()
+
+	p.Title.Text = symbol
+	p.X.Label.Text = "Time"
+	p.Y.Label.Text = fmt.Sprintf("Price, %s", currency)
+	p.X.Tick.Marker = plot.TimeTicks{Format: "2021-01-02\n15:04:05"}
+
+	bars, err := custplotter.NewCandlesticks(res)
+	if err != nil {
+		return fmt.Errorf("Error while building plot")
+	}
+
+	p.Add(bars)
+
+	// Saving graph to file, because I don't know how to work with plot bytes.
+	err = p.Save(900, 400, picFileName)
+	if err != nil {
+		return fmt.Errorf("Error while saving plot")
+	}
+
+	photoBytes, err := ioutil.ReadFile(picFileName)
+	if err != nil {
+		return fmt.Errorf("Error while sending plot")
+	}
+	photoFileBytes := tgbotapi.FileBytes{
+		Name:  "candles Graph",
+		Bytes: photoBytes,
+	}
+
+	_, err = bot.Send(tgbotapi.NewPhotoUpload(chatId, photoFileBytes))
+
 	return
 }
 
